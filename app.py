bce528b0f6b58329d948463d45c38558b17b87e6
Dmitry Ananiev <88525859+bonanzza-web@users.noreply.github.com>
2024-10-15 17:06:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
-def hello():
-  print('Hello Jenkins')
+from flask import Flask, jsonify
 
-hello()
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify(message="Hello, World!")
+
+@app.route('/api/data')
+def data():
+    return jsonify(data="Here is some data")
+
+@app.route('/user/<username>')
+def get_user(username):
+    query = f"SELECT * FROM users WHERE username = '{username}'"
+    return query
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
